f89b41b1e0236357c9de2532f7c6dcd4e1fcd206
Yuvaraj R <119421277+YuvarajRengasmy@users.noreply.github.com>
2024-11-04 14:23:00 +0530

    inite

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -29,7 +29,7 @@
         </button>
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a href="/index" class="nav-link">Home</a></li>
+                <li class="nav-item "><a href="/index" class="nav-link">Home</a></li>
                 <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
                 
                 <!-- Destinations Dropdown -->
